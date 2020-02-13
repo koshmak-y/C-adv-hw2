25991554be52336a52b5b8ce81844d627e7a327c
Evgeniy Koshmak <e.koshmak@gmail.com>
2020-02-13 14:22:35 +0200

    final

---
+++ hw2/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hw1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> list = new MyList<string>();
+            for (int i = 0; i < 20; i++)
+            {
+                list.Add("e" + i);
+            }
+            Console.WriteLine(list.GetType());
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+            Console.Write("\n");
+            Console.WriteLine(new string('-', 25));
+
+            //Console.WriteLine(list.Contains("e5"));
+            //list.Clear();
+            //list.Show();
+            string[] test = Addition.GetArray<string>(list);
+            Console.WriteLine(test.GetType());
+            for (int i = 0; i < test.Length; i++)
+            {
+                Console.Write(test[i] + " ");
+            }
+            Console.ReadKey();
+
+
+        }
+
+    }
+
+    static class Addition
+    {
+        public static T[] GetArray<T>(this MyList<T> list)
+        {
+            T[] array = new T[list.Count];
+            for (int i = 0; i < list.Count; i++)
+            {
+                array[i] = list[i];
+            }
+            return array;
+        }
+    }
+
+    public interface IMyList<T>
+    {
+        void Add(T a) { }
+        T this[int index] { get; }
+        int Count { get; }
+        void Clear();
+        bool Contains(T item);
+
+    }
+
+    public class MyList<T> : IMyList<T>
+    {
+        private T[] arr = new T[] { };
+
+        public MyList()
+        {
+            T[] arr = { };
+        }
+        public T GetValueByIndex { get; set; }
+
+        public void Add(T a)
+        {
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length - 1] = a;
+        }
+
+        public T this[int index]
+        {
+            get { return arr[index]; }
+            set { GetValueByIndex = value; }
+        }
+
+        public int Count
+        {
+            get { return arr.Length; }
+        }
+
+        public void Clear()
+        {
+            Array.Resize(ref arr, 0);
+        }
+
+        public bool Contains(T item)
+        {
+            foreach (T current in arr)
+            {
+                if ((object)current == (object)item)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void Show()
+        {
+            foreach (T item in arr)
+            {
+                Console.Write("{0} ", item);
+            }
+        }
+    }
+}
---
+++ hw3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace hw3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<int, string> list = new SortedList<int, string>();
+            list.Add(1, "one");
+            list.Add(5, "fife");
+            list.Add(10, "ten");
+            list.Add(4, "four");
+            list.Add(3, "tree");
+            list.ShowSortedUp();
+            Console.WriteLine(new string('-', 20));
+            list.ShowSortedDown();
+            Console.ReadKey();
+
+        }
+    }
+
+    public class SortedList<T, U>
+    {
+        Dictionary<T, U> List = new Dictionary<T, U>();
+
+
+        public void Add(T key, U value)
+        {
+            List.Add(key, value);
+        }
+
+        public void ShowSortedUp()
+        {
+            Console.WriteLine("SortedUp list by key:");
+            var sortedDictByOrder = List.OrderBy(x => x.Key);
+            foreach (KeyValuePair<T, U> item in sortedDictByOrder)
+            {
+                Console.WriteLine("{0} - {1}", item.Key, item.Value);
+            }
+        }
+
+        public void ShowSortedDown()
+        {
+            Console.WriteLine("SortedDown list by key:");
+            var sortedDictByOrder = List.OrderByDescending(x => x.Key);
+            foreach (KeyValuePair<T, U> item in sortedDictByOrder)
+            {
+                Console.WriteLine("{0} - {1}", item.Key, item.Value);
+            }
+        }
+    }
+}
